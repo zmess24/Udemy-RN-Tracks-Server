a051206a727be332b6afdb8968d357eee4abdda8
Zac Messsinger <zmessinger@quantummetric.com>
2019-11-19 16:21:42 -0800

    Added signin route + password hash

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const 
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,5 +13,32 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, { timestamps: true });
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) return reject(err);
+            if (!isMatch) return reject(false);
+
+            resolve(true);
+        })
+    });
+};
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next();
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next();
+
+            user.password = hash;
+            next();
+        });
+    })
+});
 
 mongoose.model('User', userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,4 +17,20 @@ router.post('/signup', async (req, res) => {
     };
 });
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.status(422).send({ error: "Must provide email and password" });
+
+    const user = await User.findOne({ email });
+    if (!user) res.status(422).send({ error: "Invalid password or email." });
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+        res.send({ token });
+    } catch (err) {
+        res.status(422).send({ error: "Invalid password or email." })
+    };
+});
+
 module.exports = router;
